df2ee8160ee029b02bd4baf9914f9200b49f61a4
caldo <gabcaldo@droidunb.com>
2024-05-01 11:06:56 -0300

    .

--- src/MotorDC.cpp
+++ src/MotorDC.cpp
@@ -23,26 +23,26 @@ MotorDC::MotorDC(const int ENCA, const int ENCB, const int PWM, const int IN1, c
 // Função para ligar o motor e definir a direção e a velocidade
 void MotorDC::ligar_motor(int dir, int pwmVal)
 {
-    analogWrite(PWM,pwmVal); // (pino do pwm, valor do pwm (máximo = 255))
-    if (dir == 1) { // 1 para frente
-      digitalWrite(IN1,HIGH);
-      digitalWrite(IN2,LOW);
-    } else if (dir == -1) { // -1 para trás
-      digitalWrite(IN1,LOW);
-      digitalWrite(IN2,HIGH);
-    } else { // 0 para parar
-      digitalWrite(IN1,LOW);
-      digitalWrite(IN2,LOW);
-    }  
+  analogWrite(PWM, pwmVal); // (pino do pwm, valor do pwm (máximo = 255))
+  if (dir == 1) { // 1 para frente
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  } else if (dir == -1) { // -1 para trás
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  } else { // 0 para parar
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  }  
 }
 
 // Função para ler o encoder do motor
 void MotorDC::ler_encoder()
 {
-    double b = digitalRead(ENCB);
-    if (b > 0) { // Se ler pulso positivo do encoder
-      posi++;
-    } else { // Se ler pulso negativo do encoder
-      posi--;
-    }
+  double b = digitalRead(ENCB);
+  if (b > 0) { // Se ler pulso positivo do encoder
+    posi++;
+  } else { // Se ler pulso negativo do encoder
+    posi--;
+  }
 }
