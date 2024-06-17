31089723ab4abe81c7852054a285a96e949d6a8b
Arthur Neves <82537546+ArthurNeves0512@users.noreply.github.com>
2024-06-16 23:03:07 -0300

    Correcao da parte integrativa

--- src/MotorDC.cpp
+++ src/MotorDC.cpp
@@ -99,11 +99,11 @@ void MotorDC::andar_reto(int velocidade_rpm)
 
   float p = kp * e;
 
-  eintegral += e;
+  eintegral += e*dt;
 
   float d = kd * ((e - eprev) / dt);
 
-  float u = p + (ki * eintegral * dt) + d; //p + (ki * eintegral*dt) + d;
+  float u = p + (ki * eintegral) + d; //p + (ki * eintegral*dt) + d;
 
   float pwmVal = fabs(u); // valor do pwm que será enviado ao motor
 
