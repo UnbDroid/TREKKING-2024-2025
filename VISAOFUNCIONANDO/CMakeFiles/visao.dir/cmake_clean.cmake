cbd2cd88bbd4193f4917813b348bed80826c5067
Arthur Neves <82537546+ArthurNeves0512@users.noreply.github.com>
2024-06-21 00:01:54 -0300

    funcao de teste que precisa ser calibrada LEMBRE QUE PRECISA SER CALIBRADA E EU PRECISO DORMIR NAO IREI FAZER ISSO AGORA PQ AMANHA PRECISARIA FAZER DNV

---
+++ VISAOFUNCIONANDO/CMakeFiles/visao.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visao.dir/visao.cpp.o"
+  "visao"
+  "visao.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
