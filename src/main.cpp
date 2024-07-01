c3602e78e895c6e5c00f51361219c2771469e9c7
caldo <gabcaldo@droidunb.com>
2024-07-01 08:33:20 -0300

    Main Arrumada

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-// Inclui a biblioteca Wire que possui as funções da comunicação I2C:
-#include "Arduino.h"
-#include <Wire.h>
-#include <MPU6050_light.h>
-#include "Volante.h"
-#include "MotorDC.h"
-#include "Pinos.h"
-#include "Robo.h"
+//! Importação das bibliotecas -----------------------------------------------------------
+
+  #include "Arduino.h"
+  #include <Wire.h>
+  #include <MPU6050_light.h>
+  #include "Volante.h"
+  #include "MotorDC.h"
+  #include "Pinos.h"
+  #include "Robo.h"
+
+//! -------------------------------------------------------------------------------------
 
 //! Criação dos objetos -----------------------------------------------------------------
 
@@ -20,9 +23,9 @@
 
 //! Declaração das variáveis de tempo ---------------------------------------------------
 
-long T;
-long prevT; 
-double dt;
+  long T;
+  long prevT; 
+  double dt;
 
 //! -------------------------------------------------------------------------------------
 
@@ -35,12 +38,9 @@ void interrupcao_encoder_direito() {
 }
 
 void ligar_robo() {
-  Serial.println("Calibrando IMU");
   Wire.begin();
   imu.begin();
   imu.calcOffsets();
-  Serial.println("Terminei de Calibrar");
-  // delay(5000);
   volante.setup();
   motor_dc_esquerdo.congirurar(2100, 1.8, 1.3, 0);
   motor_dc_direito.congirurar(2100, 3.0, 2.0, 0);
@@ -48,55 +48,38 @@ void ligar_robo() {
   attachInterrupt(digitalPinToInterrupt(ENCA_Direito), interrupcao_encoder_direito, RISING);
 }
 
-void setup() {
+void setup() { 
 
-  //! Funções de Setup -----------------------------------------
+  //! FAVOR NÃO COLOCAR FUNÇÕES DE TESTE FORA DA ÁREA DE TESTES
+
+  //* Funções de Setup -----------------------------------------
 
     Serial.begin(115200);
     
-
-    while (!Serial)
-    {
-      // volante.virar_volante(30);
-      // volante.virar_volante(-33);
-    }
+    while (!Serial) {}
 
     Serial.setTimeout(100);
 
     ligar_robo();        
 
-  //! ----------------------------------------------------------
-    // pinMode(22, OUTPUT);
-  //! Caminho do robô ------------------------------------------
-  // volante.virar_volante(30);
-  //     volante.virar_volante(-33);
-    // robo.andar_reto_cm(144);
-    // robo.virar_robo(frente, 90);
-    // Serial.println("Começando a andar");
-    robo.alinhar_com_cone(20);
-    // Serial.println(leituraVisao[0]);
-    // robo.virar_robo(frente, leituraVisao[0]);
-    // robo.andar_reto_cm(leituraVisao[1
-    // float * leituraVisao = robo.getAnguloCone();
-    // Serial.println(leituraVisao[0]);
-    // robo.virar_robo(frente, leituraVisao[0]);
-    // robo.andar_reto_cm(leituraVisao[1]);
-    // robo.virar_robo(frente,180);
-    // robo.alinhar_com_cone(55);
-    // robo.virar_robo(frente,90);
-    // robo.alinhar_com_cone(40);
+  //* ----------------------------------------------------------
 
-  //!-----------------------------------------------------------
+  //* Caminho do robô ------------------------------------------
+  
+    robo.alinhar_com_cone(20);
+
+  //* ----------------------------------------------------------
+
+  //! Funções de Teste -----------------------------------------
+
+
+  //! ----------------------------------------------------------
 
 }
  
 void loop() {
   
   //! Usar somente para testes
-  //! Deverá permanecer vazio durante as rodadas oficiais[
-  // digitalWrite(22, HIGH);
-  // delay(1000);
-  // digitalWrite(22, LOW);
-  // delay(1000);
-  
+  //! Deverá permanecer vazio durante as rodadas oficiais
+    
 }
