3f756ae81fa0dbeacc1a5e83e9ab121c94fa7daf
Arthur Neves <arthurneves0512@gmail.com>
2025-01-23 11:25:25 -0300

    Dores de cabeça e fugindo de arrumar o git da trekking

--- ROS_WS/build/rf2o_laser_odometry/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arthur/Projects/TREEKING2K24/ROS_WS/src/rf2o_laser_odometry
-# Build directory: /home/arthur/Projects/TREEKING2K24/ROS_WS/build/rf2o_laser_odometry
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
