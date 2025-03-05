dd1a779460eedae30ab296871484b49034cb8145
Arthur Neves <arthurneves0512@gmail.com>
2025-03-05 20:02:34 -0300

    prototipo da task para controlar o robo

--- components/RobotPs4Controller/RobotPs4Controller.cpp
+++ components/RobotPs4Controller/RobotPs4Controller.cpp
@@ -1,6 +1,12 @@
 #include "RobotPs4Controller.h"
 #include "MotorDC.h"
+#include "PS4BT.h"
+#include "btd_vhci.h"
+#include "esp_err.h"
+#include "freertos/idf_additions.h"
 #include "include/RobotPs4Controller.h"
+#include "iostream"
+#include <iostream>
 #include <stdio.h>
 RobotPs4Controller::RobotPs4Controller(MotorDC *right_front_motor,
                                        MotorDC *right_back_motor,
@@ -11,5 +17,36 @@ RobotPs4Controller::RobotPs4Controller(MotorDC *right_front_motor,
   this->left_front_motor = left_front_motor;
   this->left_back_motor = left_back_motor;
 };
+RobotPs4Controller::RobotPs4Controller() {}
+void RobotPs4Controller::move_foward() {
+  this->right_back_motor->set_motor(1, 110);
+  this->right_front_motor->set_motor(1, 110);
+  this->left_back_motor->set_motor(1, 110);
+  this->left_front_motor->set_motor(1, 110);
+};
+void RobotPs4Controller::set_controller(PS4BT *PS4) { this->PS4 = PS4; }
+void RobotPs4Controller::move_backward() {
 
-void RobotPs4Controller::move_foward() {};
+  this->right_back_motor->set_motor(-1, 110);
+  this->right_front_motor->set_motor(-1, 110);
+  this->left_back_motor->set_motor(-1, 110);
+  this->left_front_motor->set_motor(-1, 110);
+}
+void RobotPs4Controller::controll_robot() {
+
+  if (this->PS4->getAnalogButton(L2)) {
+    std::cout << this->PS4->getAnalogButton(L2) << std::endl;
+  }
+}
+
+void RobotPs4Controller::task_robot_controll(void *tasks_param) {
+  while (1) {
+    btd_vhci_mutex_lock();
+    controll_robot();
+    btd_vhci_mutex_unlock();
+    vTaskDelay(3);
+  }
+
+  // btd_vhci_mutex_lock();
+  // btd_vhci_autoconnect(&this->ps4);
+}
--- components/RobotPs4Controller/include/RobotPs4Controller.h
+++ components/RobotPs4Controller/include/RobotPs4Controller.h
@@ -10,18 +10,23 @@
 
 class RobotPs4Controller {
 public:
+  RobotPs4Controller();
   RobotPs4Controller(MotorDC *right_front_motor, MotorDC *right_back_motor,
                      MotorDC *left_front_motor, MotorDC *left_back_motor);
-  void move_foward();
-  void move_backward();
-  void stop();
+  void task_robot_controll(void *tasks_param);
+  void set_controller(PS4BT *PS4B);
 
 private:
   const char *mac_addr_ps4_controller = "702084757537";
+  void move_foward();
+  void move_backward();
+  void controll_robot();
+  void stop();
   MotorDC *right_front_motor;
   MotorDC *right_back_motor;
   MotorDC *left_front_motor;
   MotorDC *left_back_motor;
+  PS4BT *PS4;
 };
 
 #endif
