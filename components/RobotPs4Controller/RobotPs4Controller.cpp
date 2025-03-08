e0b171fbcca10ff592ea44c73eb3d66bab69279b
Arthur Neves <arthurneves0512@gmail.com>
2025-03-08 15:08:45 -0300

    codigo do controle sendo desenvolvido

--- components/RobotPs4Controller/RobotPs4Controller.cpp
+++ components/RobotPs4Controller/RobotPs4Controller.cpp
@@ -2,12 +2,15 @@
 #include "MotorDC.h"
 #include "PS4BT.h"
 #include "btd_vhci.h"
+#include "controllerEnums.h"
 #include "esp_err.h"
+#include "esp_log.h"
 #include "freertos/idf_additions.h"
 #include "include/RobotPs4Controller.h"
 #include "iostream"
 #include <iostream>
 #include <stdio.h>
+static const char *LOG_TAG = "main";
 RobotPs4Controller::RobotPs4Controller(MotorDC *right_front_motor,
                                        MotorDC *right_back_motor,
                                        MotorDC *left_front_motor,
@@ -33,9 +36,31 @@ void RobotPs4Controller::move_backward() {
   this->left_front_motor->set_motor(-1, 110);
 }
 void RobotPs4Controller::controll_robot() {
-
-  if (this->PS4->getAnalogButton(L2)) {
-    std::cout << this->PS4->getAnalogButton(L2) << std::endl;
+  if (this->PS4->getAnalogHat(LeftHatY) > 137 ||
+      this->PS4->getAnalogHat(LeftHatY) < 117) {
+    if (this->PS4->getAnalogButton(L2) && this->PS4->getAnalogButton(R2)) {
+      ESP_LOGI(LOG_TAG, "OS DOIS DENTRO DO WHILE Valor L2 = %d Valor R2 = %d",
+               this->PS4->getAnalogButton(L2), this->PS4->getAnalogButton(R2));
+    } else if (this->PS4->getAnalogButton(L2)) {
+      // std::cout << this->PS4->getAnalogButton(L2) << std::endl;
+      ESP_LOGI(LOG_TAG, " DENTRO DO WHILE Valor L2 = %d",
+               this->PS4->getAnalogButton(L2));
+    } else if (this->PS4->getAnalogButton(R2)) {
+      // std::cout << this->PS4->getAnalogButton(L2) << std::endl;
+      ESP_LOGI(LOG_TAG, " DENTRO DO WHILE Valor R2 = %d",
+               this->PS4->getAnalogButton(R2));
+    } else {
+      ESP_LOGI(LOG_TAG, "%d", this->PS4->getAnalogHat(LeftHatY));
+    }
+  } else if (this->PS4->getAnalogButton(L2) && this->PS4->getAnalogButton(R2)) {
+    ESP_LOGI(LOG_TAG, "Valor L2 = %d Valor R2 = %d",
+             this->PS4->getAnalogButton(L2), this->PS4->getAnalogButton(R2));
+  } else if (this->PS4->getAnalogButton(L2)) {
+    // std::cout << this->PS4->getAnalogButton(L2) << std::endl;
+    ESP_LOGI(LOG_TAG, "Valor L2 = %d", this->PS4->getAnalogButton(L2));
+  } else if (this->PS4->getAnalogButton(R2)) {
+    // std::cout << this->PS4->getAnalogButton(L2) << std::endl;
+    ESP_LOGI(LOG_TAG, "Valor R2 = %d", this->PS4->getAnalogButton(R2));
   }
 }
 
