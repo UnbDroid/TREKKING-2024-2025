8cc8c0afd10d7a6627ba8b283b99d4a37e01d946
Arthur Neves <arthurneves0512@gmail.com>
2025-03-08 18:23:53 -0300

    funcao do map feita

--- components/RobotPs4Controller/RobotPs4Controller.cpp
+++ components/RobotPs4Controller/RobotPs4Controller.cpp
@@ -14,6 +14,9 @@ static const char *LOG_TAG = "main";
 #define MAX_VALUE_PWM 160
 #define MAX_VALUE_R2_L2 255
 #define START_FOWARD_ANALOG_HAT_VALUE 116
+#define END_FOWARD_ANALOG_HAT_VALUE 0
+#define START_BACKWARD_ANALOG_HAT_VALUE 138
+#define END_BACKWARD_ANALOG_HAT_VALUE 255
 RobotPs4Controller::RobotPs4Controller(MotorDC *right_front_motor,
                                        MotorDC *right_back_motor,
                                        MotorDC *left_front_motor,
@@ -46,23 +49,21 @@ int map_R2_and_L2_to_pwm(int value) {
 int map_analogHat_foward_direction(int value) {
   int slliced_value = value * -1;
   slliced_value = slliced_value + START_FOWARD_ANALOG_HAT_VALUE;
-  int proportional = MAX_VALUE_PWM / START_FOWARD_ANALOG_HAT_VALUE;
+  float proportional = float(MAX_VALUE_PWM) / (START_FOWARD_ANALOG_HAT_VALUE -
+                                               END_FOWARD_ANALOG_HAT_VALUE);
   int new_value_scalled = proportional * slliced_value;
   return new_value_scalled;
 }
 int map_analogHat_backward_direction(int value) {
   int slliced_value = value - 138;
-  float proportional = MAX_VALUE_PWM / 255;
+  float proportional = float(MAX_VALUE_PWM) / (END_BACKWARD_ANALOG_HAT_VALUE -
+                                               START_BACKWARD_ANALOG_HAT_VALUE);
   return proportional * slliced_value;
 }
 void RobotPs4Controller::controll_robot() {
   if (this->PS4->getAnalogHat(LeftHatY) > 138 ||
       this->PS4->getAnalogHat(LeftHatY) < 116) {
-    if (this->PS4->getAnalogButton(L2) && this->PS4->getAnalogButton(R2)) {
-      ESP_LOGI(LOG_TAG, "OS DOIS DENTRO DO WHILE Valor L2 = %d Valor R2 = %d",
-               this->PS4->getAnalogButton(L2), this->PS4->getAnalogButton(R2));
-    } else if (this->PS4->getAnalogButton(L2)) {
-      // std::cout << this->PS4->getAnalogButton(L2) << std::endl;
+    if (this->PS4->getAnalogButton(L2)) {
       ESP_LOGI(LOG_TAG, " DENTRO DO WHILE Valor L2 = %d",
                this->PS4->getAnalogButton(L2));
     } else if (this->PS4->getAnalogButton(R2)) {
@@ -70,18 +71,23 @@ void RobotPs4Controller::controll_robot() {
       ESP_LOGI(LOG_TAG, " DENTRO DO WHILE Valor R2 = %d",
                this->PS4->getAnalogButton(R2));
     } else {
-      ESP_LOGI(LOG_TAG, "%d", this->PS4->getAnalogHat(LeftHatY));
+      int valor = this->PS4->getAnalogHat(LeftHatY);
+      int scalled_value_foward = map_analogHat_foward_direction(valor);
+      ESP_LOGI(LOG_TAG, "valor Y = %d valor Y mapeado = %d", valor,
+               scalled_value_foward);
     }
-  } else if (this->PS4->getAnalogButton(L2) && this->PS4->getAnalogButton(R2)) {
-    ESP_LOGI(LOG_TAG, "Valor L2 = %d Valor R2 = %d",
-             this->PS4->getAnalogButton(L2), this->PS4->getAnalogButton(R2));
   } else if (this->PS4->getAnalogButton(L2)) {
 
-    this->right_front_motor->set_motor(1, 60);
-    this->left_front_motor->set_motor(-1, 60);
+    int valor = this->PS4->getAnalogButton(L2);
+    int scalled_value = map_R2_and_L2_to_pwm(valor);
+    ESP_LOGI(LOG_TAG, " Valor L2 = %d , VALOR L2 MAPEADO = %d", valor,
+             scalled_value);
 
-    this->right_back_motor->set_motor(1, 60);
-    this->left_back_motor->set_motor(-1, 60);
+    this->right_front_motor->set_motor(1, scalled_value);
+    this->left_front_motor->set_motor(-1, scalled_value);
+
+    this->right_back_motor->set_motor(1, scalled_value);
+    this->left_back_motor->set_motor(-1, scalled_value);
 
   } else if (this->PS4->getAnalogButton(R2)) {
 
@@ -89,6 +95,7 @@ void RobotPs4Controller::controll_robot() {
     int scalled_value = map_R2_and_L2_to_pwm(valor);
     ESP_LOGI(LOG_TAG, " Valor R2 = %d , VALOR R2 MAPEADO = %d", valor,
              scalled_value);
+
     this->right_front_motor->set_motor(-1, scalled_value);
     this->left_front_motor->set_motor(1, scalled_value);
 
