cde0ac54672bb7555e86557bb634e46059691d07
Arthur Neves <arthurneves0512@gmail.com>
2025-03-27 13:54:04 -0300

    tentando fazer funcionar

--- main/main.cpp
+++ main/main.cpp
@@ -6,9 +6,12 @@
 #include "btd_vhci.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/idf_additions.h"
 #include "freertos/projdefs.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
+#include "semaphore.h"
+SemaphoreHandle_t rpmMutex;
 MotorDC left_front_motor(ENCA_LEFT_FRONT, ENCB_LEFT_FRONT, L_PWM_LEFT_FRONT,
                          R_PWM_LEFT_FRONT, LEDC_CHANNEL_LEFT_FRONT_L_PWM,
                          LEDC_CHANNEL_LEFT_FRONT_R_PWM);
@@ -34,15 +37,16 @@ void read_encoder_right_back(void *arg) { right_back_motor.read_encoder(arg); }
 
 void robot_setup() {
   pin_configuration();
-  gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
-  gpio_isr_handler_add((gpio_num_t)ENCA_LEFT_FRONT, read_encoder_left_front,
-                       (void *)ENCA_LEFT_FRONT);
-  gpio_isr_handler_add((gpio_num_t)ENCA_LEFT_BACK, read_encoder_left_back,
-                       (void *)ENCA_LEFT_BACK);
-  gpio_isr_handler_add((gpio_num_t)ENCA_RIGHT_FRONT, read_encoder_right_front,
-                       (void *)ENCA_RIGHT_FRONT);
-  gpio_isr_handler_add((gpio_num_t)ENCA_RIGHT_BACK, read_encoder_right_back,
-                       (void *)ENCA_RIGHT_BACK);
+  // gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
+  // gpio_isr_handler_add((gpio_num_t)ENCA_LEFT_FRONT, read_encoder_left_front,
+  //                     (void *)ENCA_LEFT_FRONT);
+  // gpio_isr_handler_add((gpio_num_t)ENCA_LEFT_BACK, read_encoder_left_back,
+  //                    (void *)ENCA_LEFT_BACK);
+  // gpio_isr_handler_add((gpio_num_t)ENCA_RIGHT_FRONT,
+  // read_encoder_right_front,
+  //                     (void *)ENCA_RIGHT_FRONT);
+  // gpio_isr_handler_add((gpio_num_t)ENCA_RIGHT_BACK, read_encoder_right_back,
+  //                     (void *)ENCA_RIGHT_BACK);
   left_front_motor.configure_motor(300, 1, 1, 0);
   left_back_motor.configure_motor(300, 1, 1, 0);
   right_front_motor.configure_motor(300, 2, 1, 0);
@@ -72,20 +76,21 @@ extern "C" void app_main(void) {
   ret = btd_vhci_init();
   btd_vhci_autoconnect(&PS4);
   robo.set_controller(&PS4);
-  left_front_motor.reset_encoder();
-  right_front_motor.reset_encoder();
-  left_back_motor.reset_encoder();
-  right_back_motor.reset_encoder();
+  //  left_front_motor.reset_encoder();
+  // right_front_motor.reset_encoder();
+  // left_back_motor.reset_encoder();
+  // right_back_motor.reset_encoder();
   xTaskCreatePinnedToCore(task_controll, "ps4_loop_task", 10 * 1024, NULL, 2,
                           NULL, 1);
   xTaskCreatePinnedToCore(task_velocity, "velocity", 5 * 1024, NULL, 3, NULL,
                           1);
-  float RADIO_IN_METERS = 0.06272;
-  while (1) {
-    RoboVirtual resultado = robotProperties.compute_vector_position();
-    // ESP_LOGI("robo", "distancia em x: %f", resultado.vectorPosition.x);
-    double velocidade = left_front_motor.return_speed();
-    ESP_LOGI("vel", "vel: %lf", velocidade);
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
+  // float RADIO_IN_METERS = 0.06272;
+  // while (1) {
+  //  RoboVirtual resultado = robotProperties.compute_vector_position();
+  // ESP_LOGI("robo", "distancia em x: %f", resultado.vectorPosition.x);
+  // double velocidade = left_front_motor.return_speed();
+  // ESP_LOGI("vel", "vel: %lf", velocidade);
+  //    ESP_LOGI("Vel", "%f", right_front_motor.current_speed_rpm);
+  //  vTaskDelay(pdMS_TO_TICKS(10));
+  //}
 }
